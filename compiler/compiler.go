fa664bcacff620156a7d169b313a46959c8d927a
Sean DuBois <sean@siobud.com>
2014-11-01 01:47:24 -0400

    Start compiler, just consumes file if dir scans for all jack files or can just handle a single file

---
+++ compiler/compiler.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Specifiy directory or file")
+		return
+	}
+
+	fileInfo, fileError := os.Stat(os.Args[1])
+	dirtyFilesToProcess := []string{}
+	var baseDirectory string
+
+	if fileError != nil {
+		fmt.Println("%s", fileError)
+		return
+	} else if fileInfo.IsDir() {
+		directory, _ := os.Open(os.Args[1])
+		baseDirectory = fileInfo.Name() + "/"
+		dirtyFilesToProcess, _ = directory.Readdirnames(0)
+	} else {
+		dirtyFilesToProcess = []string{fileInfo.Name()}
+		if slashIndex := strings.LastIndex(os.Args[1], "/"); slashIndex != -1 {
+			baseDirectory = os.Args[1][0:slashIndex] + "/"
+		} else {
+			baseDirectory = "."
+		}
+	}
+
+	filesToProcess := []string{}
+	for _, value := range dirtyFilesToProcess {
+		if strings.HasSuffix(value, ".jack") {
+			filesToProcess = append(filesToProcess, strings.TrimSuffix(value, ".jack"))
+		}
+	}
+	if len(filesToProcess) != 0 {
+		fmt.Printf("%s", filesToProcess)
+		fmt.Printf("%s", baseDirectory)
+	} else {
+		fmt.Println("No files to process")
+	}
+
+}
